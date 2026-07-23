a23209ea02d6b3bbcb6b44a1e5215519da2d714c
Claire Iidea <danbua999@gmail.com>
2026-07-23 20:29:08 +0700

    test. run. whatever

--- api/get-stats-image.ts
+++ api/get-stats-image.ts
@@ -5,7 +5,7 @@ import Stats from "../Stats.js";
 const Handler = async (_Req: VercelRequest, Res: VercelResponse): Promise<void> => {
     const Buffer: Buffer = await GetImg(0, 0, 24, (await Stats()).ToString("L"));
     Res.setHeader("Content-Type", "image/png")
-        .setHeader("Cache-Control", "public, max-age=3600, stale-while-revalidate=86400")
+        .setHeader("Cache-Control", "public, max-age=86400, stale-while-revalidate=86400")
         .status(200)
         .send(Buffer)
     ;
